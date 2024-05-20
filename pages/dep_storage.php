259c565011cdad9988524a3a2567e6b37bf9e190
Dmytro Sydorenko <dima83718@gmail.com>
2024-05-20 14:11:59 +0300

    apdate form 2

--- pages/dep_storage.php
+++ pages/dep_storage.php
@@ -1,13 +1,15 @@
 <?php require_once "../includes/header.php" ?>
-    <h1>Склад відділу</h1>
-    <form method="GET" action="">
-        <label for="search_med">Пошук за назвою медикаменту:</label>
-        <input type="text" id="search_med" name="search_med">
-        <button type="submit">Пошук</button>
-        <?php if (isset($_GET['id_dep'])): ?>
-            <input type="hidden" name="id_dep" value="<?= htmlspecialchars($_GET['id_dep']) ?>">
-        <?php endif; ?>
-    </form>
+<h1>Склад відділу</h1>
+
+<form method="GET" action="">
+    <label for="search_med">Пошук за назвою медикаменту:</label>
+    <input type="text" id="search_med" name="search_med">
+    <button type="submit">Пошук</button>
+    <?php if (isset($_GET['id_dep'])): ?>
+        <input type="hidden" name="id_dep" value="<?= htmlspecialchars($_GET['id_dep']) ?>">
+    <?php endif; ?>
+</form>
+
 <?php
 include_once "../database/db_connection.php";
 require_once "../includes/depart_storage.php";
@@ -49,125 +51,51 @@ if ($result->num_rows > 0) {
 $conn->close();
 ?>
 
-    <button onclick="toggleForm('transferForm')">Видати ліки</button>
-    <button onclick="toggleForm('addPersonForm')">Додати нову особу</button>
-    <div id="addPersonForm" style="display: none;">
-        <h2>Додати нову особу</h2>
-        <form method="post" action="../includes/add_person.php">
-            Ім'я: <input type="text" name="name" required><br><br>
-            Прізвище: <input type="text" name="surname" required><br><br>
-            По батькові: <input type="text" name="patronim" required><br><br>
-            Вік: <input type="date" name="age" required><br><br>
-            Адреса: <input type="text" name="address" required><br><br>
-            Телефон: <input type="text" name="telephone" required><br><br>
-            <input type="submit" value="Додати особу">
-        </form>
-    </div>
-    <div id="transferForm" style="display: none;">
-        <h1>Передача медикаменту клієнту</h1>
-        <form method="post" action="process_transfer_med_to_client.php">
-            <h2>Виберіть медикамент, особу та співробітника</h2>
+<?php if ($_SESSION['login'] !== 'admin'): ?>
+<button onclick="toggleForm('transferForm')">Видати ліки</button>
+<button onclick="toggleForm('addPersonForm')">Додати нову особу</button>
+<div id="addPersonForm" style="display: none;">
+    <h2>Додати нову особу</h2>
+    <form method="post" action="../includes/add_person.php">
+        Ім'я: <input type="text" name="name" required><br><br>
+        Прізвище: <input type="text" name="surname" required><br><br>
+        По батькові: <input type="text" name="patronim" required><br><br>
+        Вік: <input type="date" name="age" required><br><br>
+        Адреса: <input type="text" name="address" required><br><br>
+        Телефон: <input type="text" name="telephone" required><br><br>
+        <input type="submit" value="Додати особу">
+    </form>
+</div>
+<div id="transferForm" style="display: none;">
+    <h1>Передача медикаменту клієнту</h1>
+    <form method="post" action="process_transfer_med_to_client.php">
+        <h2>Виберіть медикамент, особу та співробітника</h2>
 
-            Медикамент:
-            <input type="text" id="medicineInput" name="medicineInput" required>
-            <input type="hidden" id="id_med_client" name="id_med_client">
-            <div id="autocomplete-medicine-list" class="autocomplete-suggestions"></div><br><br>
+        Медикамент:
+        <input type="text" id="medicineInput" name="medicineInput" required>
+        <input type="hidden" id="id_med_client"name="id_med_client">
+        <div id="autocomplete-medicine-list" class="autocomplete-suggestions"></div><br><br>
 
-            Особа:
-            <input type="text" id="personInput" name="personInput" required>
-            <input type="hidden" id="id_person" name="id_person">
-            <div id="autocomplete-person-list" class="autocomplete-suggestions"></div><br><br>
+        Особа:
+        <input type="text" id="personInput" name="personInput" required>
+        <input type="hidden" id="id_person" name="id_person">
+        <div id="autocomplete-person-list" class="autocomplete-suggestions"></div><br><br>
 
-            Кількість: <input type="number" name="count_to_person" required><br><br>
-            Дата: <input type="date" name="date_output_to_client" required><br><br>
-            <input type="submit" value="Передати медикамент клієнту">
-        </form>
-    </div>
+        Кількість: <input type="number" name="count_to_person" required><br><br>
+        Дата: <input type="date" name="date_output_to_client" required><br><br>
+        <input type="submit" value="Передати медикамент клієнту">
+    </form>
+</div>
+<?php endif; ?>
 
-
-    <script>
-        function toggleForm() {
-            const form = document.getElementById('transferForm');
-            if (form.style.display === 'none' || form.style.display === '') {
-                form.style.display = 'block';
-            } else {
-                form.style.display = 'none';
-            }
+<script>
+    function toggleForm(formId) {
+        var form = document.getElementById(formId);
+        if (form.style.display === 'none') {
+            form.style.display = 'block';
+        } else {
+            form.style.display = 'none';
         }
-
-        document.getElementById('medicineInput').addEventListener('input', function() {
-            const query = this.value;
-
-            if (query.length >= 2) {
-                const xhr = new XMLHttpRequest();
-                xhr.open('GET', '../includes/get_medicines.php?query=' + query, true);
-                xhr.onload = function() {
-                    if (this.status === 200) {
-                        const medicines = JSON.parse(this.responseText);
-                        const autocompleteList = document.getElementById('autocomplete-medicine-list');
-                        autocompleteList.innerHTML = '';
-
-                        medicines.forEach(medicine => {
-                            const item = document.createElement('div');
-                            item.classList.add('autocomplete-suggestion');
-                            item.textContent = `${medicine.name_med} ${medicine.med_form} ${medicine.dosage} ${medicine.producer}`;
-                            item.dataset.id = medicine.id_med;
-                            item.addEventListener('click', function() {
-                                document.getElementById('medicineInput').value = this.textContent;
-                                document.getElementById('id_med_client').value = this.dataset.id;
-                                autocompleteList.innerHTML = '';
-                            });
-                            autocompleteList.appendChild(item);
-                        });
-                    }
-                }
-                xhr.send();
-            } else {
-                document.getElementById('autocomplete-medicine-list').innerHTML = '';
-            }
-        });
-
-        document.getElementById('personInput').addEventListener('input', function() {
-            const query = this.value;
-
-            if (query.length >= 2) {
-                const xhr = new XMLHttpRequest();
-                xhr.open('GET', 'get_persons.php?query=' + query, true);
-                xhr.onload = function() {
-                    if (this.status === 200) {
-                        const persons = JSON.parse(this.responseText);
-                        const autocompleteList = document.getElementById('autocomplete-person-list');
-                        autocompleteList.innerHTML = '';
-
-                        persons.forEach(person => {
-                            const item = document.createElement('div');
-                            item.classList.add('autocomplete-suggestion');
-                            item.textContent = person.full_name;
-                            item.dataset.id = person.id_person;
-                            item.addEventListener('click', function() {
-                                document.getElementById('personInput').value = this.textContent;
-                                document.getElementById('id_person').value = this.dataset.id;
-                                autocompleteList.innerHTML = '';
-                            });
-                            autocompleteList.appendChild(item);
-                        });
-                    }
-                }
-                xhr.send();
-            } else {
-                document.getElementById('autocomplete-person-list').innerHTML = '';
-            }
-        });
-
-    </script>
-    <script>
-        function toggleForm(formId) {
-            var form = document.getElementById(formId);
-            if (form.style.display === 'none') {
-                form.style.display = 'block';
-            } else {
-                form.style.display = 'none';
-            }
-        }
-    </script>
+    }
+</script>
 <?php require_once "../includes/footer.php" ?>
